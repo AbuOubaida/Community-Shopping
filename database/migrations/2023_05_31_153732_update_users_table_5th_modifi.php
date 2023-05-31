dd44aac51df3385d956e8514381bb99ee6c1888a
abuoubaida <abuoubaida36@gmail.com>
2023-05-31 23:05:47 +0600

    super admin user data modify

---
+++ database/migrations/2023_05_31_153732_update_users_table_5th_modifi.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('status',function ($table){
+                $table->integer('delete_status')->default(0)->comment('1 = deleted','0 = not delete');
+            });
+            $table->unsignedBigInteger('update_by')->nullable();
+            $table->foreign('update_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
