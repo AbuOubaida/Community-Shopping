192f8a82c876426b52d2fadbc831aad400757839
abuoubaida <abuoubaida36@gmail.com>
2023-07-06 00:43:36 +0600

    magnified effect on product image and discount store on DB

---
+++ database/migrations/2023_07_05_182225_update_order_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            //
+            $table->after('total_price',function ($table){
+                $table->string('discount')->nullable();
+                $table->string('discount_total')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            //
+        });
+    }
+};
